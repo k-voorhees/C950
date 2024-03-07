b09531b0b9f46819f30cffb34ea314e9170f6fbd
Kason <kasonjvoorhees@gmail.com>
2024-03-07 13:15:01 -0800

    create main() with planning comments

---
+++ main.py
@@ -0,0 +1,14 @@
+def main():
+    # import data from files
+
+    # create hash table
+
+    # create Trucks
+
+    # load truck
+
+    # deliver packages
+    pass
+
+if __name__ == "__main__":
+    main()
